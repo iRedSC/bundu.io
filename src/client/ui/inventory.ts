ff873dcb4e33480997e5365ce897df5eb8be390e
iRedSC <redstonecharger@gmail.com>
2023-12-10 18:15:03 -0500

    Removed hold inventory.

--- src/client/ui/inventory.ts
+++ src/client/ui/inventory.ts
@@ -1,105 +1,105 @@
-import * as PIXI from 'pixi.js';
-import { InventoryButton } from './button';
-
-
+import * as PIXI from "pixi.js";
+import { InventoryButton } from "./button";
 
 type Item = { imagePath: string; result: string; amount: number };
 
-
 export class Inventory {
-  container: PIXI.Container
-  slots: InventoryButton[]
+    container: PIXI.Container;
+    slots: InventoryButton[];
 
-  constructor() {
-    this.container = new PIXI.Container()
-    this.slots = [];
-  }
-
-  addSlot(item?: Item) {
-    const button = new InventoryButton();
-    if (item) {
-      button.setItem(item);
+    constructor() {
+        this.container = new PIXI.Container();
+        this.slots = [];
     }
-    button.view.x = this.slots.length * (inventorySlotSize + padding);
-    this.slots.push(button)
-    this.container.addChild(button.view);
-    button.view.on("pointerdown", () => dragStart(button))
-  }
 
-
-    
-
-  remove() {
-    for (let button of this.slots) {
-      this.slots= []
-      this.container.removeChild(button.view);
+    addSlot(item?: Item) {
+        const button = new InventoryButton();
+        if (item) {
+            button.setItem(item);
+        }
+        button.view.x = this.slots.length * (inventorySlotSize + padding);
+        this.slots.push(button);
+        this.container.addChild(button.view);
+        button.view.on("pointerdown", () => dragStart(button));
     }
-  }
+
+    remove() {
+        for (let button of this.slots) {
+            this.container.removeChild(button.view);
+        }
+        this.slots = [];
+    }
 }
-
 
 const inventorySlotSize = 60;
 const inventorySlotCount = 10;
 const padding = 6;
 
 export const inventory = new Inventory();
-export const localInventoryOrder: (Item)[] = [];
-
+export let localInventoryOrder: Item[] = [];
 
 function resize() {
-  inventory.container.position.set(
-    (window.innerWidth - inventorySlotSize * inventorySlotCount) / 2,
-    window.innerHeight - inventorySlotSize - 10)
+    inventory.container.position.set(
+        (window.innerWidth - inventorySlotSize * inventorySlotCount) / 2,
+        window.innerHeight - inventorySlotSize - 10
+    );
 }
 window.addEventListener("resize", resize);
-resize()
+resize();
 
 const invItems: Item[] = [
-  {
-    imagePath: "./assets/gold_wall.svg",
-    result: "Selected Item 1",
-    amount: 2,
-  },
-  {
-    imagePath: "./assets/meat.svg",
-    result: "Selected Item 2",
-    amount: 3,
-  },
-  {
-    imagePath: "./assets/gold_hammer.svg",
-    result: "Selected Item 3",
-    amount: 4,
-  },
-  {
-    imagePath: "./assets/stone.svg",
-    result: "Selected Item 4",
-    amount: 9,
-  }];
+    {
+        imagePath: "./assets/gold_wall.svg",
+        result: "Selected Item 1",
+        amount: 2,
+    },
+    {
+        imagePath: "./assets/meat.svg",
+        result: "Selected Item 2",
+        amount: 3,
+    },
+    {
+        imagePath: "./assets/gold_hammer.svg",
+        result: "Selected Item 3",
+        amount: 4,
+    },
+    {
+        imagePath: "./assets/stone.svg",
+        result: "Selected Item 4",
+        amount: 5,
+    },
+    {
+        imagePath: "./assets/gold_helmet.svg",
+        result: "Selected Item 4",
+        amount: 6,
+    },
+    {
+        imagePath: "./assets/gold_sword.svg",
+        result: "Selected Item 4",
+        amount: 7,
+    },
+    {
+        imagePath: "./assets/diamond_pickaxe.svg",
+        result: "Selected Item 4",
+        amount: 8,
+    },
+    {
+        imagePath: "./assets/earmuffs.svg",
+        result: "Selected Item 4",
+        amount: 9,
+    },
+];
 
+localInventoryOrder = structuredClone(invItems);
 
-  function holdinventory(newItems: Item[] = [], ) {
-    for (const newItem of newItems) {
-      const existingIndex = localInventoryOrder.findIndex(item => item.result === newItem.result);
-  
-      if (existingIndex !== -1) {
-        localInventoryOrder[existingIndex] = { ...localInventoryOrder[existingIndex], ...newItem };
-      } else {
-        localInventoryOrder.push(newItem);
-      }
-
-      updateinventory()
+updateinventory();
+function updateinventory() {
+    inventory.remove();
+    for (let i = 0; i < inventorySlotCount; i++) {
+        const item = localInventoryOrder[i];
+        console.log(item);
+        inventory.addSlot(item);
     }
-  }
-
-holdinventory(invItems,)
-
-function updateinventory(){
-inventory.remove()
-for (let i = 0; i < inventorySlotCount; i++) {
-  const item = localInventoryOrder[i];
-
-  inventory.addSlot(item);
-}
 }
 
 function dragStart(button: InventoryButton) {
@@ -109,17 +109,15 @@ function dragStart(button: InventoryButton) {
     sprite.alpha = 0.8;
     const _dragMove = (event: PointerEvent) => dragMove(sprite, event);
 
-  function dragEnd() {
-    window.removeEventListener('pointermove', _dragMove);
-    window.removeEventListener('pointerup', dragEnd);
-    inventory.container.removeChild(sprite);
-    findswap(button)
-  }
+    function dragEnd() {
+        window.removeEventListener("pointermove", _dragMove);
+        window.removeEventListener("pointerup", dragEnd);
+        inventory.container.removeChild(sprite);
+        findswap(button);
+    }
 
-  window.addEventListener('pointermove', _dragMove);
-  window.addEventListener('pointerup', dragEnd);
- 
-
+    window.addEventListener("pointermove", _dragMove);
+    window.addEventListener("pointerup", dragEnd);
 }
 
 function dragMove(sprite: PIXI.Sprite, event: PointerEvent) {
@@ -135,31 +133,29 @@ function dragMove(sprite: PIXI.Sprite, event: PointerEvent) {
     sprite.position.set(pos.x, pos.y);
 }
 
-function findswap(button: InventoryButton){
-  for(let item of inventory.slots) {
-    if (item.hovering) {
-      const currentButton = inventory.slots.indexOf(item);
-      const oldButton = inventory.slots.indexOf(button);
+function findswap(button: InventoryButton) {
+    for (let item of inventory.slots) {
+        if (item.hovering) {
+            const currentButton = inventory.slots.indexOf(item);
+            const oldButton = inventory.slots.indexOf(button);
 
-      const oldItem = localInventoryOrder[oldButton];
+            const oldItem = localInventoryOrder[oldButton];
 
-      localInventoryOrder.splice(oldButton, 1);
-      localInventoryOrder.splice(currentButton, 0, oldItem);
- 
+            localInventoryOrder.splice(oldButton, 1);
+            localInventoryOrder.splice(currentButton, 0, oldItem);
 
-      for (let i = localInventoryOrder.length - 1; i >= 0; i--) {
-        const currentItem = localInventoryOrder[i];
-      
-        if (!currentItem) {
-          localInventoryOrder.push(localInventoryOrder.splice(i, 1)[0]);
+            for (let i = localInventoryOrder.length - 1; i >= 0; i--) {
+                const currentItem = localInventoryOrder[i];
+
+                if (!currentItem) {
+                    localInventoryOrder.push(
+                        localInventoryOrder.splice(i, 1)[0]
+                    );
+                }
+            }
+
+            updateinventory();
+            console.log(inventory.slots);
         }
-      }
-      
-
-      updateinventory()
-      console.log(inventory.slots)
     }
-  }
 }
-
-
